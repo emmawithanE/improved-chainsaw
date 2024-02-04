dd73ef5b2e74c94ab40b1077da2e0bab78a63c15
emmawithanE <emma@catgirl.software>
2024-02-04 22:40:25 +1030

    sticky_notes: Generate colours from a bucket source

--- sticky_notes/sticky_notes.gd
+++ sticky_notes/sticky_notes.gd
@@ -1,9 +1,20 @@
 extends Control
 
 var note : PackedScene = preload("res://sticky_notes/sticky_note.tscn")
+var colour_source : BucketSource
 
+const COLOUR_COUNT : float = 12
+
+func _ready():
+	colour_source = BucketSource.new()
+	var colour_array : Array[Color] = []
+	for i in range(COLOUR_COUNT):
+		var hue = float(i) / COLOUR_COUNT
+		colour_array.append(Color.from_hsv(hue, 0.2, 1))
+	colour_source.fill(colour_array)
+	
 func _on_button_button_down():
 	var inst = note.instantiate()
 	add_child(inst)
-	inst.init()
+	inst.init(colour_source.draw())
 	
